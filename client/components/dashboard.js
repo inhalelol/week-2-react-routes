7b4be9d5e9ba550f800de17476d51fdda7055426
inhalelol <medvedev.sasha1998@gmail.com>
2020-10-24 02:58:38 +0300

    fix id=title

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -8,9 +8,9 @@ const Dashboard = () => {
       <Head title="Hello" />
       <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
         Dashboard
-        <div><Link to="/dashboard/profile/5f1a172f-fef4-48a6-954b-50e9f266eabc">Go to Profile</Link></div>
-        <div><Link to="/dashboard/main">Go to Main</Link></div>
       </div>
+      <div><Link to="/dashboard/profile/5f1a172f-fef4-48a6-954b-50e9f266eabc">Go to Profile</Link></div>
+      <div><Link to="/dashboard/main">Go to Main</Link></div>
     </div>
   )
 }
--- client/components/main.js
+++ client/components/main.js
@@ -8,10 +8,9 @@ const Main = () => {
       <Head title="Hello" />
       <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
         Main
-        <div><Link to="/dashboard/profile/5f1a172f-fef4-48a6-954b-50e9f266eabc">Go to Profile</Link></div>
-        <div><Link to="/dashboard/">Go to Root</Link></div>
       </div>
-
+      <div><Link to="/dashboard/profile/5f1a172f-fef4-48a6-954b-50e9f266eabc">Go to Profile</Link></div>
+      <div><Link to="/dashboard/">Go to Root</Link></div>
     </div>
   )
 }
